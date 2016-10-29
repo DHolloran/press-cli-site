98046c92f9954c6d3ce4cf4927c57bb9f727cf8f
Dan Holloran <dtholloran@gmail.com>
2016-10-29 16:16:38 -0500

    Sets up production

--- source/_partials/github-buttons.blade.php
+++ source/_partials/github-buttons.blade.php
@@ -1,8 +1,11 @@
 <ul class="github-buttons">
     <li>
-        <a class="github-button" href="https://github.com/dholloran/press-cli/issues" aria-label="Issue dholloran/press-cli on GitHub">Issue</a>
+        <a class="github-button" href="https://github.com/dholloran/press-cli" data-icon="octicon-star" aria-label="Star dholloran/press-cli on GitHub">Star</a>
     </li>
     <li>
-        <a class="github-button" href="https://github.com/dholloran/press-cli" aria-label="Star dholloran/press-cli on GitHub">Star</a>
+        <a class="github-button" href="https://github.com/dholloran/press-cli" data-icon="octicon-eye" aria-label="Watch dholloran/press-cli on GitHub">Watch</a>
+    </li>
+    <li>
+        <a class="github-button" href="https://github.com/dholloran/press-cli/issues" data-icon="octicon-issue-opened" aria-label="Issue dholloran/press-cli on GitHub">Issue</a>
     </li>
 </ul>
